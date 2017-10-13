9ccc6ccb7f1e429e5cad1f8a29b2c172e7565c90
take79 <tk.hrsk79@gmail.com>
2017-10-13 17:30:20 +0900

    compute absolute position of each node

--- calc_pos.py
+++ calc_pos.py
@@ -1,25 +1,29 @@
 import numpy as np
 import math
+import sys
 
-with open ('hierarchy.txt', 'r') as f:
-  hie = f.readlines()
+with open (sys.argv[1], 'r') as f:
+  raw = f.readlines()
+
+  hierarchy = raw[0:311]
+  frames = raw[312:]
 
   joint_offsets = []
   joint_names = []
 
-  for idx, line in enumerate(hie):
-    hie[idx] = hie[idx].split()
-    print(hie[idx])
-    if not len(hie[idx]) == 0:
-      line_type = hie[idx][0]
+  for idx, line in enumerate(hierarchy):
+    hierarchy[idx] = hierarchy[idx].split()
+    #print(hierarchy[idx])
+    if not len(hierarchy[idx]) == 0:
+      line_type = hierarchy[idx][0]
       if line_type == 'OFFSET':
-        offset = [float(hie[idx][1]), float(hie[idx][2]), float(hie[idx][3])]
+        offset = np.array([float(hierarchy[idx][1]), float(hierarchy[idx][2]), float(hierarchy[idx][3])])
         joint_offsets.append(offset)
       elif line_type == 'ROOT' or line_type == 'JOINT':
-        joint_names.append(hie[idx][1])
+        joint_names.append(hierarchy[idx][1])
       elif line_type == 'End':
         joint_names.append('End Site')
-  
+
   nodes = []
   for idx, name in enumerate(joint_names):
     if idx == 0:
@@ -35,26 +39,76 @@ with open ('hierarchy.txt', 'r') as f:
     else:
       parent = idx - 1
 
-    node = dict([('name', name), ('parent', parent), ('offsets', joint_offsets[idx])])
+    if name == 'End Site':
+      children = None
+    elif idx == 0: #hips
+      children = [1, 54, 59]
+    elif idx == 2: #spine1
+      children = [3, 6, 30]
+    elif idx == 9: #lefthand
+      children = [10, 14, 18, 22, 26]
+    elif idx == 33: #righthand
+      children = [34, 38, 42, 46, 50]
+    else:
+      children = [idx + 1]
+
+    node = dict([('name', name), ('parent', parent), ('children', children), ('offset', joint_offsets[idx]), ('rel_pos', None), ('abs_pos', None)])
+    if idx == 0:
+        node['rel_pos'] = node['abs_pos'] = [float(0), float(60), float(0)]
     nodes.append(node)
 
-  #xrot_mat = np.matrix([1, 0, 0], [0, np.cos(np.radians(x)), -np.sin(np.radians(x))], [0, np.sin(np.radians(x)), np.cos(np.radians(x))])
-  #yrot_mat = np.matrix([np.cos(np.radians(y)), 0, np.sin(np.radians(y))], [0, 1, 0], [-np.sin(np.radians(y)), 0, np.cos(np.radians(y))])
-  #zrot_mat = np.matrix([np.cos(np.radians(z)), -np.sin(np.radians(z)), 0], [np.sin(np.radians(z)), np.cos(np.radians(z)), 0], [0, 0, 1])
+  for idx, frame in enumerate(frames):
+    frames[idx] = frame.split()
 
-  print(joint_offsets)
+  relatives = []
+  node_idx = 0
+  for i in range(51):
+    stepi = i*3
+    z_deg = float(frames[0][stepi])
+    x_deg = float(frames[0][stepi+1])
+    y_deg = float(frames[0][stepi+2])
+
+    xrot_mat = np.matrix([[1, 0, 0], [0, np.cos(np.radians(x_deg)), -np.sin(np.radians(x_deg))], [0, np.sin(np.radians(x_deg)), np.cos(np.radians(x_deg))]])
+    yrot_mat = np.matrix([[np.cos(np.radians(y_deg)), 0, np.sin(np.radians(y_deg))], [0, 1, 0], [-np.sin(np.radians(y_deg)), 0, np.cos(np.radians(y_deg))]])
+    zrot_mat = np.matrix([[np.cos(np.radians(z_deg)), -np.sin(np.radians(z_deg)), 0], [np.sin(np.radians(z_deg)), np.cos(np.radians(z_deg)), 0], [0, 0, 1]])
+
+    if nodes[node_idx]['name'] == 'End Site':
+      node_idx = node_idx + 1
+    current_node = nodes[node_idx]
+    print(current_node['name'])
+
+    for child_idx in current_node['children']:
+      child_node = nodes[child_idx]
+      child_offset = np.array([child_node['offset']]).T
+      child_node['rel_pos'] = np.dot(zrot_mat, np.dot(yrot_mat, np.dot(xrot_mat, child_offset))).A1
+
+    node_idx = node_idx + 1
+
+  for start_node in nodes:
+    abs_pos = np.array([0, 0, 0])
+    current_node = start_node
+    print(">" + current_node['name'])
+    while current_node['parent'] is not None:
+      print(nodes[current_node['parent']]['name'] + "->")
+      abs_pos = abs_pos + current_node['rel_pos']
+      current_node = nodes[current_node['parent']]
+    start_node['abs_pos'] = abs_pos
+    print("origin")
+    print("-------------------------------------------")
+
+  #print(joint_offsets)
   print(len(joint_offsets))
   print(joint_names)
   print(len(joint_names))
   for node in nodes:
       print(node)
 
-  for i in range(len(nodes)):
-    current_node = nodes[i]
-    print(">" + current_node['name'])
-    while current_node['parent'] is not None:
-      print(nodes[current_node['parent']]['name'] + "->")
-      current_node = nodes[current_node['parent']]
-    print("origin")
-    print("-------------------------------------------")
+  #for i in range(len(nodes)):
+  #  current_node = nodes[i]
+  #  print(">" + current_node['name'])
+  #  while current_node['parent'] is not None:
+  #    print(nodes[current_node['parent']]['name'] + "->")
+  #    current_node = nodes[current_node['parent']]
+  #  print("origin")
+  #  print("-------------------------------------------")
 
